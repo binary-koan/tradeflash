a67878c49692069811310ceb959cfb9194b3e7e6
Jono Mingard <reason.koan@gmail.com>
2015-08-01 02:45:09 +0000

    Basic socket.io functionality

--- client/app/app.js
+++ client/app/app.js
@@ -1,36 +1,23 @@
 (function(){
   'use strict';
 
-  initialize('title');
-
-  function initialize(elementId){
-
-    var runner = Runner(elementId);
-    console.log(runner)
-    runner.update('Check out HackReactor');
-
-    ///////////////////////
-
-    function Runner(elementId){
-      var instance;
-
-      instance = {
-
-        element: document.getElementById(elementId),
-        update: update
-
-      };
-
-      return instance;
-
-      //////////////////
-
-      function update(value){
-        this.element.innerHTML = value;
-      }
-    }
-
-
-  }
+  var socket = io();
+  
+  socket.on('update', function(msg) {
+    console.log("Updated! " + msg);
+  });
 
 })();
+
+
+$(function() {
+    $(".auctionImage")
+        .mouseover(function() { 
+            $(this).attr("width","125");
+            $(this).attr("height","125");
+        })
+        .mouseout(function() {
+             $(this).attr("width","100");
+            $(this).attr("height","100");
+        });
+});

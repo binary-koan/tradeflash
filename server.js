b69212082d19cc90901fc22626212c429faea61a
Jono Mingard <reason.koan@gmail.com>
2015-08-01 11:28:17 +0000

    Added OpenShift settings

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var app = require('./server/app')();
 var http = require('http').Server(app);
 require('./server/socket')(http);
 
-var port = process.env.PORT || 9000;
-var ip = process.env.IP || '0.0.0.0';
+var port = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 9000;
+var ip = process.env.OPENSHIFT_NODEJS_IP || process.env.IP || '0.0.0.0';
 
 http.listen(port, ip, function(){
   console.log('App Listening on ' + ip + ':' + port);

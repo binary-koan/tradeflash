7ec20f97f44989eb2d02965f0931a80ad9a28532
Jono Mingard <reason.koan@gmail.com>
2015-08-01 10:19:56 +0000

    Add testing pages to public

---
+++ public/js/app.js
@@ -0,0 +1,93 @@
+(function() {
+  var socket = io();
+  
+  var ROUTES = {
+    index: '/',
+    images: '/flow'
+  };
+  
+  var StartPage = {
+    controller: function() { return {} },
+    
+    view: function(ctrl) {
+      return m("a", { href: ROUTES.images, config: m.route }, 'Default images');
+    }
+  };
+
+  var Images = {
+    list: m.prop([]),
+    
+    setup: function() {
+      socket.on('update', function(msg) {
+        if (m.route() != ROUTES.images) return;
+        
+        console.log(msg);
+        Images.list().push(msg.url);
+        Images.list().shift();
+        m.redraw();
+      });
+      
+      socket.on('sending items', function(items) {
+        if (m.route() != ROUTES.images) return;
+        
+        console.log('sending items');
+        Images.list(items);
+        m.redraw();
+      });
+    },
+    
+    controller: function() {
+      socket.emit('get items');
+      
+      return {
+        list: Images.list
+      };
+    },
+    
+    view: function(ctrl) {
+      return m('.row', ctrl.list().map(function(src) {
+        return m('.col-md-4.col-xs-6', m('.thumb',
+          m('a.thumbnail.listing', { href: '#' }, [
+            m('img.img-responsive', { src: src }),
+            m('.overlay'),
+            m('.pricetag', [
+              m('span.sr-only', 'Current bid: '),
+              m('span', '$10.00')
+            ]),
+            m('h5.title', 'Item title')
+          ])
+        ));
+      }));
+    }
+  };
+  
+  Images.setup();
+  
+  var routeControllers = {};
+  routeControllers[ROUTES.index] = StartPage;
+  routeControllers[ROUTES.images] = Images;
+  
+  m.route(document.getElementById('main'), ROUTES.index, routeControllers);
+})();
+
+$(function() {
+    $(".thumbnail")
+        .mouseover(function() {
+           // $(this).text("sfdasfasdfdas");
+           $(".title").html("sweatshirt");
+        })
+        .mouseout(function() {
+             $(".title").html("");
+        });
+});
+
+$(function() {
+    $(".catagory")
+        .click(function() { 
+          alert("clicked");
+        })
+        .mouseout(function() {
+             $(".title").html("");
+        });
+});
+
---
+++ public/js/start.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#button2")
+        .click(function() {
+           // $(this).text("sfdasfasdfdas");
+          // $(".title").html("sweatshirt");
+         // $(this).addClass("current").siblings().removeClass("active");
+         alert("clicked");
+        })
+        .mouseout(function() {
+            /// $(".title").html("");
+        });
+});

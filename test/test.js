799a3fe341e5d642886fae95f383cfb328eb964a
Jono Mingard <reason.koan@gmail.com>
2015-08-01 10:21:32 +0000

    Start work on tests

--- test/test.js
+++ test/test.js
@@ -2,3 +2,8 @@ var app = require('express')();
 var http = require('http');
 var https = require('https');
 
+// Test 1: Make sure we get a 200 OK from a call to TradeMe API
+
+// Test 2: Make sure our JSON response has been filtered properly (only relevant fields)
+
+// Test 3: Test interval function is ticking properly

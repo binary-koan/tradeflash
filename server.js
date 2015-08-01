42af0b75ca4ff6e58e2c8d9f145e5a7260ce364b
Jono Mingard <reason.koan@gmail.com>
2015-08-01 10:46:19 +0000

    Listen on 0.0.0.0 by default

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 require('./server/socket')(http);
 
 var port = process.env.PORT || 9000;
-var ip = process.env.IP || 'localhost';
+var ip = process.env.IP || '0.0.0.0';
 
 http.listen(port, ip, function(){
   console.log('App Listening on ' + ip + ':' + port);

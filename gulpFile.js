6cb4db3e372867176c707dc934806784f935a957
Jono Mingard <reason.koan@gmail.com>
2015-08-01 09:22:19 +0000

    Use browserify and less to compile assets

--- gulpFile.js
+++ gulpFile.js
@@ -1,42 +1,52 @@
-(function(){
-  var gulp  = require('gulp');
-  var $     = require('gulp-load-plugins')({lazy:false});
+var gulp  = require('gulp');
+var $ = require('gulp-load-plugins')({ lazy: false });
+var watchify = require('watchify');
+var browserify = require('browserify');
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var gutil = require('gulp-util');
+var assign = require('lodash.assign');
 
-$.livereload();
-$.livereload.listen();
+gulp.task('default', $.sequence('js', 'less', 'server'));
 
-var paths = {
-  index: './client/index.html',
-  root: './client',
-  html: './client/**/*.html',
-  scripts: './client/app/**/*.js',
-  styles: './client/app/**/*.css'
+// Assets
+
+var customOpts = {
+  entries: ['./assets/scripts/main.js'],
+  debug: true
+};
+var opts = assign({}, watchify.args, customOpts);
+var b = watchify(browserify(opts));
+
+var bundle = function() {
+  return b.bundle()
+    // log errors if they happen
+    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+    .pipe(source('main.js'))
+    // optional, remove if you don't need to buffer file contents
+    .pipe(buffer())
+    // optional, remove if you dont want sourcemaps
+    .pipe($.sourcemaps.init({loadMaps: true})) // loads map from browserify file
+       // Add transformation tasks to the pipeline here.
+    .pipe($.sourcemaps.write('./')) // writes .map file
+    .pipe(gulp.dest('./public'));
 }
 
-gulp.task('default', $.sequence('inject', 'server', 'watch'));
-gulp.task('server', startServer);
-gulp.task('watch', startWatch);
-gulp.task('inject', startInject);
+gulp.task('js', bundle); // so you can run `gulp js` to build the file
+b.on('update', bundle); // on any dep update, runs the bundler
+b.on('log', gutil.log); // output build logs to terminal
 
-function startServer(){
+gulp.task('less', function() {
+  return gulp.src('./assets/styles/main.less')
+    .pipe($.less())
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.watch('./assets/styles/**/*.less', ['less']);
+
+// Server
+
+gulp.task('server', function() {
   require('./server');
-
-}
-function startWatch(){
-  gulp.watch('./client/app/**/*.css', $.livereload.changed);
-  gulp.watch('./client/app/**/*.js', $.livereload.changed);
-  gulp.watch('./client/**/*.html', $.livereload.changed);
-}
-
-function startInject(){
-  var target  = gulp.src( paths.index );
-  var scripts = gulp.src( paths.scripts, {read:false} );
-  var styles  = gulp.src( paths.styles, {read:false} );
-
-  return target
-    .pipe( $.inject( scripts,  {relative:true}) )
-    .pipe( $.inject( styles,  {relative:true}) )
-    .pipe( gulp.dest( paths.root ) );
-}
-
-})();
+});
